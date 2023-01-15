3e1558fd016c6ac0396ffa7232f978c53179dedc
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-15 23:49:41 +0000

    centering home icons

--- pages/index.js
+++ pages/index.js
@@ -85,7 +85,7 @@ export default function Index() {
             <div className='text-gray-600 font-semibold px-2 text-center text-lg'>BRANDING</div>
         </div>
         <div className='flex flex-row object-center items-center justify-center gap-8 p-10'>
-            <div><button type='button' onClick={() => router.push('/Contact')} className='text-black bg-gray-500 p-2 font-bold text-md rounded-md flex'><MdOutlineRequestQuote className='text-2xl align-middle mr-2'/>Get Quotes</button></div>
+            <div><button type='button' onClick={() => router.push('/Contact')} className='text-black bg-gray-500 p-2 font-bold text-md rounded-md flex align-middle justify-center'><MdOutlineRequestQuote className='text-2xl align-middle mr-2'/>Get Quotes</button></div>
             <div><button type='button' onClick={() => router.push('/Contact')} className='text-black bg-gray-500 p-2 font-bold text-md rounded-md flex'><BiPhoneCall className='text-2xl align-middle mr-2'/>Get Quotes</button></div>
         </div>
         </div>
