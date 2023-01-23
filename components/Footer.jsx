94ec10b14f537f0e8c6e2f654807127eb7a6df56
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-23 23:11:49 +0000

    adds link to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,6 +6,7 @@ function Footer() {
         <div className='flex text-white text-sm text-center justify-center px-10'>
         Copyright ©2020-2026 AD Portal
         </div>
+        <a href="https://myportfolio-beta-rouge.vercel.app/"><div className='text-xs text-center pt-1 text-white flex justify-center cursor-pointer '>Powered by Emmanuel Wediah</div></a>
     </div>
   )
 }
