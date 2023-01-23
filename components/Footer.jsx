ba6618b1828007ae00b04fc871e9954782d79b4f
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-23 23:21:12 +0000

    adds hover effects to signature

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <div className='flex text-white text-sm text-center justify-center px-10'>
         Copyright ©2020-2026 AD Portal
         </div>
-        <a href="https://myportfolio-beta-rouge.vercel.app/"><div className='text-xs text-center pt-1 text-white flex justify-center cursor-pointer '>Powered by Emmanuel Wediah</div></a>
+        <a href="https://myportfolio-beta-rouge.vercel.app/"><div className='hover:text-black text-xs text-center pt-1 text-white flex justify-center cursor-pointer font-mono hover:underline-offset-1 hover:underline hover:decoration-2'>Powered by Emmanuel Wediah</div></a>
     </div>
   )
 }
