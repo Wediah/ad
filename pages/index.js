e037037e6e4cf54ce1cab2cd92485dc61031ebe7
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-23 22:49:42 +0000

    changes animations to fade-up

--- pages/index.js
+++ pages/index.js
@@ -358,7 +358,7 @@ export default function Index() {
                         <h2 className='text-black text-2xl font-semibold text-center pt-20 pb-2'>Who We Are</h2>
                         <p className='flex text-gray-500 text-xl text-center justify-center'>We give life to brands. Design a unique voice for them. Position them on the market in a way that they attract the right amount of attention.</p> 
 
-                        <div data-aos='zoom-in' className='flex flex-wrap gap-5 lg:flex-rol lg:flex-wrap pt-20'>
+                        <div data-aos='fade-up' className='flex flex-wrap gap-5 lg:flex-rol lg:flex-wrap pt-20'>
                             <div className='text-center p-10 my-10 flex-1 cursor-pointer hover:shadow-xl'>
                                 <h1 className='text-6xl font-bold text-black'>100</h1> <br />
                                 <div className='font-semibold text-gray-500'>Happy Clients</div>
