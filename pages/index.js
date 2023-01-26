c25ba231bec119a438ebb65ecd4eacc9c2af8e57
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-26 00:05:57 +0000

    form removed

--- pages/index.js
+++ pages/index.js
@@ -38,43 +38,6 @@ export default function Index() {
 
   const router = useRouter()
 
-      // Handles the submit event on form submit.
-      const handleSubmit = async (event) => {
-        // Stop the form from submitting and refreshing the page.
-        event.preventDefault()
-    
-        // Get data from the form.
-        const data = {
-          first: event.target.first.value,
-          last: event.target.last.value,
-        }
-    
-        // Send the data to the server in JSON format.
-        const JSONdata = JSON.stringify(data)
-    
-        // API endpoint where we send form data.
-        const endpoint = '/api/form'
-    
-        // Form the request for sending data to the server.
-        const options = {
-          // The method is POST because we are sending data.
-          method: 'POST',
-          // Tell the server we're sending JSON.
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          // Body of the request is the JSON data we created above.
-          body: JSONdata,
-        }
-    
-        // Send the form data to our forms API on Vercel and get a response.
-        const response = await fetch(endpoint, options)
-    
-        // Get the response data from server as JSON.
-        // If server returns the name submitted, that means the form works.
-        const result = await response.json()
-        alert(`Is this your full name: ${result.data}`)
-      }
 
   return (
     <>
